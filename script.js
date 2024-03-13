9cdade1d09aa1f4e69746f56561fc5a3d6215318
Sebastian <jedynytakisebek@gmail.com>
2024-03-13 14:37:01 +0100

    added functions for speed and color buttons

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const squares = 546;
 let sliderValue = 70;
 let range = 360;
 
-const createSquare = () =>
-{
-    const squaresStack = [];
+const createSquare = speed =>
+{   box.innerHTML = ""
 
     for (let i = 0; i < squares; i++)
     {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.transitionDuration = speed;
 
         square.addEventListener("mouseover", () => setColor(square));
         square.addEventListener("mouseout", () => removeColor(square));
@@ -49,6 +49,23 @@ const setColor = square =>
 
 const removeColor = square => {
 	square.style.backgroundColor = `transparent`
+};
+
+function handleSpeed() {
+	const newSpeed = this.dataset.speed + "s";
+	createSquare(newSpeed);
+};
+
+function handleColorRange() {
+	range = parseInt(this.dataset.colorRange)
+};
+
+const showSliderInfo = () => {
+    sliderValue = slider.value;
+    sliderInfo.textContent = slider.value;
 }
 
+speedBtns.forEach(btn => btn.addEventListener("click", handleSpeed));
+colorBtns.forEach(btn => btn.addEventListener("click", handleColorRange));
+slider.addEventListener('mousemove', showSliderInfo)
 createSquare();
